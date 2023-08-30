8dd977c47865ab4c2767e6b8a31db883aecc5bbd
salman <alysalman15@gmail.com>
2023-08-30 13:29:09 +0500

    jS assignment chap 17-20

---
+++ javaScript/Chap 17-20/app.js
@@ -0,0 +1,78 @@
+//<-------- Question-no-01-------->
+
+// var arr = [[], [], []];
+
+//<-------- Question-no-02-------->
+
+// var num = [
+//   [0, 1, 2, 3],
+//   [1, 0, 1, 2],
+//   [2, 1, 0, 1],
+// ];
+// for (var i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+//<-------- Question-no-03-------->
+
+// var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+//<-------- Question-no-04-------->
+
+// var num = +prompt("Enter Number to show its multiplication table");
+// var length = +prompt("Enter length table");
+
+// for (var i = 1; i <= length; i++) {
+//   var multiply = num * i;
+//   document.write(num + "x" + i + "=" + multiply + "<br>");
+// }
+
+//<-------- Question-no-05-------->
+
+// var fruits = ["apple", "banana", "mango", "orange", "mango"];
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// for (var i = 0; i < fruits.length; i++) {
+//   console.log("Elements at index " + i + " is " + fruits[i]);
+// }
+
+//<-------- Question-no-06-------->
+
+// (a)
+// Counting
+
+// var count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (var i = 0; i < count.length; i++) {
+//   console.log(count[i]);
+// }
+
+// (b)
+// Reverse Counting
+
+// for (var i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// (c)
+// Even
+
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// (d)
+// Odd
+
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
