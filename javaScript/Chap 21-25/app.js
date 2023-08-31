28387c601be39217884f3772d907c860b92e60f9
salman <alysalman15@gmail.com>
2023-08-31 20:34:42 +0500

    Chap 21-25

---
+++ javaScript/Chap 21-25/app.js
@@ -0,0 +1,15 @@
+// <-------Questions-01-------->
+
+// var firstName = prompt("Enter your First Name");
+// var lastName = prompt("Enter your Last Name");
+
+// fullName = firstName + " " + lastName;
+
+// console.log("Welcome " + fullName + " to Gilgit Baltistan");
+
+// <-------Questions-02-------->
+
+// var favMobile = prompt("Enter your favorite mobile name");
+
+// console.log(favMobile.length);
+
