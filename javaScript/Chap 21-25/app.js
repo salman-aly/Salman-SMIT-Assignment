d7ecbbe0f8a5cb05cc0d30fde273800edd9dbcbb
salman <alysalman15@gmail.com>
2023-09-03 21:59:02 +0500

    jS assignment 21-15

--- javaScript/Chap 21-25/app.js
+++ javaScript/Chap 21-25/app.js
@@ -13,3 +13,26 @@
 
 // console.log(favMobile.length);
 
+// <-------Questions-03-------->
+
+// var str = "Pakistani";
+
+// var index = str.indexOf("n");
+
+// console.log(index);
+
+// <-------Questions-04-------->
+
+// var str = "Hello world";
+
+// var index = str.lastIndexOf("l");
+
+// console.log(index);
+
+// <-------Questions-05-------->
+
+// var str = "Pakistani";
+
+// var index = str[3];
+
+// console.log(index);
