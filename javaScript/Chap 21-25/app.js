277bad1511a61b9094f381b9855576ccbd0611ef
salman <alysalman15@gmail.com>
2023-09-04 17:18:54 +0500

    jS assignment

--- javaScript/Chap 21-25/app.js
+++ javaScript/Chap 21-25/app.js
@@ -36,3 +36,37 @@
 // var index = str[3];
 
 // console.log(index);
+
+// <-------Questions-06-------->
+
+// var firstName = prompt("Enter your First Name");
+// var lastName = prompt("Enter your Last Name");
+
+// var fullName = firstName.concat(" " + lastName);
+
+// console.log("Welcome " + fullName + " to Gilgit Baltistan");
+
+// <-------Questions-07-------->
+
+// var city="Hyderabad";
+
+// document.write("City "+ city)
+// document.write("<br/>After replacement: "+ city.replace("Hyder","Islam"))
+
+// <-------Questions-08-------->
+
+// var message =
+//   "Ali and Sami are best friends. They play cricket and football together.";
+
+// document.write(message.replaceAll("and", "&"));
+
+// <-------Questions-09-------->
+
+// var value = "472";
+
+// document.write("Value: " + value);
+// document.write("<br/>Type: " + typeof value);
+// document.write("<br/>Value: " + value);
+// document.write("<br/>Type: " + typeof +(value));
+
+// <-------Questions-10-------->
