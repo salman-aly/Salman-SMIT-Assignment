5f592c90be7b59a897d6fa642b7bd9539c08638c
salman <alysalman15@gmail.com>
2023-09-08 01:33:29 +0500

    jS assignment chap 26-30

---
+++ javaScript/Chap 26-30/app.js
@@ -0,0 +1,70 @@
+// <--------Question--#01---------->
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+
+// document.write(
+//   "Number :" +
+//     num +
+//     "<br/>" +
+//     "round value :" +
+//     round +
+//     "<br/>" +
+//     "floor Vlaue :" +
+//     floor +
+//     "<br/>" +
+//     "Ceil value :" +
+//     ceil
+// );
+
+// <--------Question--#02---------->
+
+// var num = -2.673;>
+// var round = Math.round(num);
+// var floor = Math.floor(num);
+// var ceil = Math.ceil(num);
+
+// document.write(
+//   "Number : " +
+//     num +
+//     "<br/>" +
+//     "round value : " +
+//     round +
+//     "<br/>" +
+//     "floor Vlaue : " +
+//     floor +
+//     "<br/>" +
+//     "Ceil value : " +
+//     ceil
+// );
+
+// <--------Question--#03---------->
+
+// var num = -4;
+
+// var absolute = Math.abs(num);
+// console.log("The absolute value of " + num + " is " + absolute);
+
+// <--------Question--#04---------->
+
+// var dice = Math.floor(Math.random() * 8);
+// console.log("Random dice value : " + dice);
+
+// <--------Question--#05---------->
+
+// var random = Math.floor(Math.random() * 5);
+// if (random == 2) {
+//   console.log("Head");
+// } else {
+//   console.log("Tail");
+// }
+
+// <--------Question--#06---------->
+
+// var random = Math.floor(Math.random() * 100);
+
+// console.log("Random between 1 to 100 : " + random);
+
+// <--------Question--#07---------->
+
+// continue...
