19e8897de6de1770fa78a3dde1286c264cc5930d
salman <alysalman15@gmail.com>
2023-09-09 02:15:00 +0500

    31-34 continue..

---
+++ javaScript/Chap 31-34/app.js
@@ -0,0 +1,55 @@
+// <--------question-#01-------->
+
+// var date = new Date();
+// console.log(date);
+
+// <--------question-#02-------->
+
+// var months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December",
+// ];
+
+// var date = new Date();
+// var now = date.getMonth();
+// var monthName = months[now];
+// console.log("Current month : " + monthName);
+
+// <--------question-#03-------->
+
+// var week = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+// var date = new Date();
+// var weekName = date.getDay();
+
+// var now = week[weekName];
+
+// console.log(now);
+
+// <--------question-#04-------->
+
+// var week = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+
+// var userInput = prompt("Enter day name").toLowerCase();
+
+// if (userInput === "saturday" || userInput === "sunday") {
+//   console.log("Its fun day");
+// }
