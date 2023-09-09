98c79a430752324e7021aa22693c988f0f0cd935
salman <alysalman15@gmail.com>
2023-09-09 19:31:14 +0500

    update

--- javaScript/Chap 31-34/app.js
+++ javaScript/Chap 31-34/app.js
@@ -53,3 +53,36 @@
 // if (userInput === "saturday" || userInput === "sunday") {
 //   console.log("Its fun day");
 // }
+
+// <--------question-#05-------->
+
+// var month = new Date();
+
+// var now = month.getDate();
+
+// if (now <= 15) {
+//   console.log("First fifteen days of month");
+// } else {
+//   console.log("Last days of month");
+// }
+
+// <--------question-#06-------->
+
+var now = new Date();
+
+var past = new Date("Jan 1 1970");
+
+var getMiliSec = now - past;
+
+var getMin = getMiliSec / 60000;
+
+document.write(
+  "Current date : " +
+    now.toDateString() +
+    "<br/>" +
+    "Elapsed milisecond since Jan 1, 1970 : " +
+    getMiliSec +
+    "<br/>" +
+    "Elapsed Minutes since Jan 1, 1970 : " +
+    getMin
+);
