5a4327ada9aacc1f0b179bcac185f33e4ed8c059
salman <alysalman15@gmail.com>
2023-09-12 03:56:31 +0500

    update

--- javaScript/Chap 35-38/app.js
+++ javaScript/Chap 35-38/app.js
@@ -9,12 +9,24 @@
 
 // >>>>>>>>>>Question No 02<<<<<<<<<<
 
-function fullName(fname, lname) {
-  var fullName = fname + " " + lname;
-  document.write("Welcome " + fullName);
-}
+// var firstName = prompt("Enter your first name here");
+// var lastName = prompt("Enter your last name here");
 
-var firstName = prompt("Enter your first name here");
-var lastName = prompt("Enter your last name here");
+// function fullName(fname, lname) {
+//   var fullName = fname + " " + lname;
+//   document.write("Welcome " + fullName);
+// }
 
-fullName(firstName, lastName);
+// fullName(firstName, lastName);
+
+// >>>>>>>>>>Question No 03<<<<<<<<<<
+
+// var number1 = +prompt("Enter first number");
+// var number2 = +prompt("Enter second number");
+
+// function add(num1, num2) {
+//   var result = num1 + num2;
+//   document.write("The total is : " + result);
+// }
+
+// add(number1, number2);
